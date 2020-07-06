91f8bea4a81c7603baa21f9a17b14368fcecd127
yodaka <engineeryodaka@gmail.com>
2020-07-06 15:07:18 +0900

    section3まで

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -2,7 +2,7 @@
 //  ContentView.swift
 //  Landmarks
 //
-//  Created by 藤田泰輔 on 2020/07/06.
+//  Created by yodaka on 2020/07/06.
 //  Copyright © 2020 yodaka. All rights reserved.
 //
 
@@ -10,7 +10,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park.")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+        .padding()
     }
 }
 
